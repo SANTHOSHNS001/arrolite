6790f725b26ec2684ad3a374dce9d04f02d8ac26
SANTHOSHNS001 <santhoshnarayanan932000@gmail.com>
2026-03-17 17:28:50 +0530

    update requi

--- update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home/santhosh01/arrolite
-source /home/santhosh01/venv/bin/activate
-git pull
-python manage.py collectstatic --noinput
-pa_reload_webapp santhosh01.pythonanywhere.com
